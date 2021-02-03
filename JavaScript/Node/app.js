99c3fedba374cb78a4c3a809aaf963689ef59906
Yesman Choque <yesman.choque@gmail.com>
2021-02-03 16:42:34 -0300

    Parsing Request Bodies

--- JavaScript/Node/app.js
+++ JavaScript/Node/app.js
@@ -12,11 +12,21 @@ const server = http.createServer((req, res) => {
         return res.end(); 
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.text', 'PROGRAMMING');
+        const body = [];
+        req.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            console.log(message);
+            fs.writeFileSync('message.txt', message);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
-    }
+    };
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
