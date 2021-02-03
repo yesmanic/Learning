0fb2dc7bad7fa9af0abb9c318cb0666e4ee1cc5b
Yesman Choque <yesman.choque@gmail.com>
2021-02-03 16:32:22 -0300

    Routing Requests

--- JavaScript/Node/app.js
+++ JavaScript/Node/app.js
@@ -1,9 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
-    res.serHeader('Content-Type', 'text/html');
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end(); 
+    }
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
