c969f855984dc182ab6c4e2766cba20d97bc4c6f
Yesman Choque <yesman.choque@gmail.com>
2021-02-06 19:54:49 -0300

    Filtering Paths

--- JavaScript/Node/Working with Express.js/routes/admin.js
+++ JavaScript/Node/Working with Express.js/routes/admin.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-	res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="sumbit">Add product</button></form>');
+	res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="sumbit">Add product</button></form>');
 });
 
+// /admin/add-product => POST
 router.post('/product', (req, res, next) => {
 	console.log(req.body);
 	res.redirect('/');
