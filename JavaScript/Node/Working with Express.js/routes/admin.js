7b0a78715f15586ba1e61fd4583578e45a975037
Yesman Choque <yesman.choque@gmail.com>
2021-02-07 14:01:29 -0300

    Using a Helper Function for Navigation

--- JavaScript/Node/Working with Express.js/routes/admin.js
+++ JavaScript/Node/Working with Express.js/routes/admin.js
@@ -2,15 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
 	console.log(req.body);
 	res.redirect('/');
 });
