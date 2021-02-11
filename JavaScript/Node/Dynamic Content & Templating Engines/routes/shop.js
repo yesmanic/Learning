802a9da96fff823fe6694a59df4566f457f45718
Yesman Choque <yesman.choque@gmail.com>
2021-02-11 18:56:24 -0300

    Adding the Layout to Handlebars

--- JavaScript/Node/Dynamic Content & Templating Engines/routes/shop.js
+++ JavaScript/Node/Dynamic Content & Templating Engines/routes/shop.js
@@ -10,7 +10,14 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
 	const products = adminData.products;
 	console.log(products);
-	res.render('shop', { prods: products, pageTitle: 'Shop', path: '/', hasProducts: products.length > 0 });
+	res.render('shop', { 
+		prods: products, 
+		pageTitle: 'Shop', 
+		path: '/', 
+		hasProducts: products.length > 0,
+		activeShop: true,
+		productCSS: true
+	});
 });
 
 module.exports = router;
