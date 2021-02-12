533cad3d734823bc6ec568c53ccf3670634908db
Yesman Choque <yesman.choque@gmail.com>
2021-02-12 20:04:02 -0300

    Finishing the Controllers

--- JavaScript/Node/The Model View Controller (MVC)/app.js
+++ JavaScript/Node/The Model View Controller (MVC)/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -17,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	res.status(404).render('404', { pageTitle: 'Page not found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
