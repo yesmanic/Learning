a8514a5cb3eecb80c581e8351fb1e6962320c11e
Yesman Choque <yesman.choque@gmail.com>
2021-02-13 14:55:33 -0300

    Refactoring the File Storage Code

--- JavaScript/Node/The Model View Controller (MVC)/models/product.js
+++ JavaScript/Node/The Model View Controller (MVC)/models/product.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
+const { get } = require('https');
 const path = require('path');
+
+const p = path.join(
+	path.dirname(require.main.filename),
+	'data',
+	'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			cb([]);
+		} else {
+			cb(JSON.parse(fileContent));
+		};
+	});
+};
 
 module.exports = class Product {
 	constructor(t) {
@@ -7,16 +24,7 @@ module.exports = class Product {
 	};
 
 	save() {
-		const p = path.join(
-			path.dirname(require.main.filename),
-			'data',
-			'products.json'
-		);
-		fs.readFile(p, (err, fileContent) => {
-			let products = [];
-			if (!err) {
-				products = JSON.parse(fileContent);
-			};
+		getProductsFromFile((products) => {
 			products.push(this);
 			fs.writeFile(p, JSON.stringify(products), err => {
 				console.log(err);
@@ -25,16 +33,6 @@ module.exports = class Product {
 	};
 
 	static fetchAll(cb) {
-		const p = path.join(
-			path.dirname(require.main.filename),
-			'data',
-			'products.json'
-		);
-		fs.readFile(p, (err, fileContent) => {
-			if (err) {
-				cb([]);
-			};
-			cb(JSON.parse(fileContent));
-		});
+		getProductsFromFile(cb);
 	};
 };
