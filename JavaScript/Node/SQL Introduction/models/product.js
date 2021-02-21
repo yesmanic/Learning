9a51290ce0ca1bbe1c99d8d6286adf9c482dbb45
Yesman Choque <yesman.choque@gmail.com>
2021-02-21 15:11:25 -0300

    Fetching Products

--- JavaScript/Node/SQL Introduction/models/product.js
+++ JavaScript/Node/SQL Introduction/models/product.js
@@ -1,3 +1,5 @@
+const db = require('../util/database');
+
 const fs = require('fs');
 const path = require('path');
 
@@ -29,44 +31,18 @@ module.exports = class Product {
     }
 
     save() {
-        getProductsFromFile(products => {
-            if (this.id) {
-                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
-                const updatedProducts = [...products];
-                updatedProducts[existingProductIndex] = this;
-                fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                    console.log(err);
-                });
-            } else {
-                this.id = Math.random().toString();
-                products.push(this);
-                fs.writeFile(p, JSON.stringify(products), err => {
-                    console.log(err);
-                });
-            }
-        });
+        
     }
 
     static deleteById(id) {
-        getProductsFromFile(products => {
-            const product = products.find(prod => prod.id === id);
-            const updatedProducts = products.filter(prod => prod.id !== id);
-            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-                if (!err) {
-                    Cart.deleteProduct(id, product.price);
-                };  
-            });
-        });
+        
     };
 
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
+    static fetchAll() {
+        return db.execute('SELECT * FROM products')
     }
 
-    static findById(id, cb) {
-        getProductsFromFile(products => {
-            const product = products.find(p => p.id === id);
-            cb(product);
-        });
+    static findById(id) {
+        
     }
 };
