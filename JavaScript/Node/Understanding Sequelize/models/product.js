f80094c704cf461a6a7a30a6ce36a92f0d8b6389
Yesman Choque <yesman.choque@gmail.com>
2021-02-22 18:06:49 -0300

    Defining a Model

--- JavaScript/Node/Understanding Sequelize/models/product.js
+++ JavaScript/Node/Understanding Sequelize/models/product.js
@@ -1,51 +1,27 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const fs = require('fs');
-const path = require('path');
+const sequelize = require('../util/database');
 
-const Cart = require('./cart');
-
-const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'
-);
-
-const getProductsFromFile = cb => {
-    fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            cb([]);
-        } else {
-            cb(JSON.parse(fileContent));
-        }
-    });
-};
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id,
-            this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
+const Product = sequelize.define('product', {
+    id: {
+        type: Sequelize.INTEGER, 
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: Sequelize.STRING,
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    descripion: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
+});
 
-    save() {
-        return db.execute(
-            'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-            [this.title, this.price, this.imageUrl, this.description]
-        );
-    };
-
-    static deleteById(id) {
-        
-    };
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products')
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-    }
-};
+module.exports = Product;
