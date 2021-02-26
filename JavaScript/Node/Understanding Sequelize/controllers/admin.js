b8b1bc5bb1b9ecc8234c1ff9450d4c1edb737365
Yesman Choque <yesman.choque@gmail.com>
2021-02-26 19:45:32 -0300

    Fetching Admin Products

--- JavaScript/Node/Understanding Sequelize/controllers/admin.js
+++ JavaScript/Node/Understanding Sequelize/controllers/admin.js
@@ -65,13 +65,22 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
+    Product.findAll()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products'
+            });
+        })
+        .catch(err => console.log(err))
+   /*  Product.fetchAll(products => {
         res.render('admin/products', {
             prods: products,
             pageTitle: 'Admin Products',
             path: '/admin/products'
         });
-    });
+    }); */
 };
 
 exports.postDeleteProduct = (req, res, next) => {
