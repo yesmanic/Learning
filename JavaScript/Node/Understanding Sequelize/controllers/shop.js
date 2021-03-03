e8185683ab7a6887fc04574031772ccd5a21123b
Yesman Choque <yesman.choque@gmail.com>
2021-03-03 18:39:24 -0300

    Adding New Products to the Cart

--- JavaScript/Node/Understanding Sequelize/controllers/shop.js
+++ JavaScript/Node/Understanding Sequelize/controllers/shop.js
@@ -55,7 +55,6 @@ exports.getCart = (req, res, next) => {
     req.user
         .getCart()
         .then(cart => {
-            console.log(cart)
             return cart
                 .getProducts()
                 .then(products => {
@@ -72,10 +71,35 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-        Cart.addProduct(prodId, product.price);
-    });
-    res.redirect('/cart');
+    let fetchedCart;
+    req.user
+        .getCart()
+        .then(cart => {
+            fetchedCart = cart;
+            return cart.getProducts({ where: { id: prodId } })
+        })
+        .then(products => {
+            let product;
+            if (products.lenght > 0) {
+                product = products[0];
+            }
+            let newQuantity = 1;
+            if (product) {
+                // ...
+            }
+            return Product.findByPk(prodId)
+                .then(product => {
+                    return fetchedCart
+                        .addProduct(product, 
+                            { through: { quantity: newQuantity} }
+                        )
+                })
+                .catch(err => console.log(err));
+        })
+        .then(() => {
+            res.redirect('/cart')
+        })
+        .catch(err => console.log(err))
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
