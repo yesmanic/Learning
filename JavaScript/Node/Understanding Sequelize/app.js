cb8fb02f9c181cc3b7f7898433f4dd4a27675f12
Yesman Choque <yesman.choque@gmail.com>
2021-03-03 18:26:14 -0300

    Creating & Fetching a Cart

--- JavaScript/Node/Understanding Sequelize/app.js
+++ JavaScript/Node/Understanding Sequelize/app.js
@@ -37,14 +37,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
-User.hasMany(Cart);
+User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-    .sync({ force: true })
-    //.sync()
+    //.sync({ force: true })
+    .sync()
     .then(result => {
         return User.findByPk(1);
     })
@@ -56,6 +56,9 @@ sequelize
     })
     .then(user => {
         //console.log(user);
+        return user.createCart();
+    })
+    .then(cart => {
         app.listen(3000);
     })
     .catch(err => {
