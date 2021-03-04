fccaec5e4b4ca3db0b4922da22c3f3874ef167fd
Yesman Choque <yesman.choque@gmail.com>
2021-03-04 15:25:42 -0300

    Adding an Order Model

--- JavaScript/Node/Understanding Sequelize/app.js
+++ JavaScript/Node/Understanding Sequelize/app.js
@@ -17,6 +17,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,6 +43,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem })
 
 sequelize
     //.sync({ force: true })
