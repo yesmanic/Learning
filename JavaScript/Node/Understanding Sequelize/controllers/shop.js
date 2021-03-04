a4602c28bffb0f39c99742eb929e52de3213b5fe
Yesman Choque <yesman.choque@gmail.com>
2021-03-04 15:00:53 -0300

    Adding Existing Products

--- JavaScript/Node/Understanding Sequelize/controllers/shop.js
+++ JavaScript/Node/Understanding Sequelize/controllers/shop.js
@@ -72,29 +72,30 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
     let fetchedCart;
+    let newQuantity = 1;
     req.user
         .getCart()
         .then(cart => {
             fetchedCart = cart;
-            return cart.getProducts({ where: { id: prodId } })
+            return cart.getProducts({ where: { id: prodId } });
         })
         .then(products => {
             let product;
-            if (products.lenght > 0) {
+            if (products.length > 0) {
                 product = products[0];
             }
-            let newQuantity = 1;
+            
             if (product) {
-                // ...
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity + 1;
+                return product
             }
-            return Product.findByPk(prodId)
-                .then(product => {
-                    return fetchedCart
-                        .addProduct(product, 
-                            { through: { quantity: newQuantity} }
-                        )
-                })
-                .catch(err => console.log(err));
+            return Product.findByPk(prodId)  
+        })
+        .then(product => {
+            return fetchedCart.addProduct(product, {
+                through: { quantity: newQuantity }
+            });
         })
         .then(() => {
             res.redirect('/cart')
