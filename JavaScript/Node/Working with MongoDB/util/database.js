d3790ee90d25ffaa6e87c9522ca2cb6c00319df5
Yesman Choque <yesman.choque@gmail.com>
2021-03-06 11:00:44 -0300

    Finishing the Database Connection

--- JavaScript/Node/Working with MongoDB/util/database.js
+++ JavaScript/Node/Working with MongoDB/util/database.js
@@ -1,15 +1,30 @@
 const mongodb = require('mongodb');
+const { get } = require('../routes/admin');
 const MongoClient = mongodb.MongoClient;
+
+let _db;
 
 const config = require('./config.json');
 
 const mongoConnect = callback => {
-    MongoClient.connect(`mongodb+srv://yesman:${config.password}@cluster0.qpcbs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    MongoClient.connect(`mongodb+srv://yesman:${config.password}@cluster0.qpcbs.mongodb.net/shop?retryWrites=true&w=majority`)
         .then(client => {
             console.log('Connected!');
-            callback(client);
+            _db = client.db()
+            callback();
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
