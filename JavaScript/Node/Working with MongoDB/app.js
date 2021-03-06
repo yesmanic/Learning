fded58ae1dd551f0ddfb79be0949742f3422bff5
Yesman Choque <yesman.choque@gmail.com>
2021-03-06 10:48:41 -0300

    Creating the Database Connection

--- JavaScript/Node/Working with MongoDB/app.js
+++ JavaScript/Node/Working with MongoDB/app.js
@@ -11,8 +11,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-/* const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop'); */
+const adminRoutes = require('./routes/admin');
+//const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,8 +26,8 @@ app.use((req, res, next) => {
         .catch(err => console.log(err)); */
 })
 
-/* app.use('/admin', adminRoutes);
-app.use(shopRoutes); */
+app.use('/admin', adminRoutes);
+//app.use(shopRoutes);
 
 app.use(errorController.get404);
 
