75342dcc192624088289a84f5e5fa128d6e8ff55
Yesman Choque <yesman.choque@gmail.com>
2021-03-08 18:33:37 -0300

    Finishing the Update Product Code

--- JavaScript/Node/Working with MongoDB/controllers/admin.js
+++ JavaScript/Node/Working with MongoDB/controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require('mongodb');
 const Product = require('../models/product');
+
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -47,29 +50,29 @@ exports.getEditProduct = (req, res, next) => {
         .catch(err => console.log(err));
 };
 
-
-/*
 exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    Product.findByPk(prodId)
-        .then(product => {
-            product.title = updatedTitle;
-            product.price = updatedPrice;
-            product.description = updatedDesc;
-            product.imageUrl = updatedImageUrl;
-            return product.save();
-        })
+
+    const product = new Product(
+        updatedTitle, 
+        updatedPrice, 
+        updatedDesc, 
+        updatedImageUrl, 
+        new ObjectId(prodId)
+    )
+    product
+        .save()    
         .then(result => {
             console.log('UPDATED PRODUCT!');
             res.redirect('/admin/products');
         })
         .catch(err => console.log(err))
 }
-*/
+
 exports.getProducts = (req, res, next) => {
     Product.fetchAll()
         .then(products => {
