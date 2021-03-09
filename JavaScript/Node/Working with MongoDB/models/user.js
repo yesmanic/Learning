4c3717e9029e5a3dbdfcb10f1191b47be27136a4
Yesman Choque <yesman.choque@gmail.com>
2021-03-09 18:43:59 -0300

    Working on Cart Items

--- JavaScript/Node/Working with MongoDB/models/user.js
+++ JavaScript/Node/Working with MongoDB/models/user.js
@@ -4,9 +4,11 @@ const database = require('../util/database');
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.username = username,
-        this.email = email
+        this.email = email,
+        this.cart = cart,
+        this._id = id
     }
 
     save() {
@@ -14,6 +16,21 @@ class User {
         return db
             .collection('users')
             .insertOne(this)
+    }
+
+    addToCart(product) {
+        /* const cartProduct = this.cart.items.findIndex(cp => {
+            return cp._id === product._id;
+        }) */
+
+        const updatedCart = { items: [{ ...product, quantity: 1 }] }
+        const db = database.getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: updatedCart } }
+            )
     }
 
     static findById(userId) {
