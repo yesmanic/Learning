83e20b7e112be75ae8d4b03d4f8237b10c95abd9
Yesman Choque <yesman.choque@gmail.com>
2021-03-09 18:13:02 -0300

    Creating New Users

--- JavaScript/Node/Working with MongoDB/models/user.js
+++ JavaScript/Node/Working with MongoDB/models/user.js
@@ -1,22 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const database = require('../util/database');
 
-const sequelize = require('../util/database');
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
+class User {
+    constructor(username, email) {
+        this.username = username,
+        this.email = email
     }
-})
+
+    save() {
+        const db = database.getDb();
+        return db
+            .collection('users')
+            .insertOne(this)
+    }
+
+    static findById(userId) {
+        const db = database.getDb();
+        return db
+            .collection('users')
+            .findOne({ _id: new ObjectId(userId) })
+    }
+}
 
 module.exports = User;
