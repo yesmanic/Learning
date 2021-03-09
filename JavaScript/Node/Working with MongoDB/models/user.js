ee1cb1f77bc6e7c361c91ef4d39d082ab48929f1
Yesman Choque <yesman.choque@gmail.com>
2021-03-09 18:27:51 -0300

    Storing the User in the Database

--- JavaScript/Node/Working with MongoDB/models/user.js
+++ JavaScript/Node/Working with MongoDB/models/user.js
@@ -21,6 +21,11 @@ class User {
         return db
             .collection('users')
             .findOne({ _id: new ObjectId(userId) })
+            .then(user => {
+                console.log(user);
+                return user;
+            })
+            .catch(err => console.log(err))
     }
 }
 
