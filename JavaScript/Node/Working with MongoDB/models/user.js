1e0d6e9898588a97be3e0667e9166770239e2287
Yesman Choque <yesman.choque@gmail.com>
2021-03-11 16:58:32 -0300

    Displaying the Cart Items

--- JavaScript/Node/Working with MongoDB/models/user.js
+++ JavaScript/Node/Working with MongoDB/models/user.js
@@ -42,6 +42,27 @@ class User {
             )
     }
 
+    getCart() {
+        const db = database.getDb();
+        const productIds = this.cart.items.map(i => {
+            return i.productId;
+        });
+        return db
+            .collection('products')
+            .find({_id: {$in: productIds}})
+            .toArray()
+            .then(products => {
+                return products.map(p => {
+                    return {
+                        ...p, 
+                        quantity: this.cart.items.find(i => {
+                            return i.productId.toString() === p._id.toString();
+                        }).quantity
+                    }
+                })
+            });
+    }
+
     static findById(userId) {
         const db = database.getDb();
         return db
