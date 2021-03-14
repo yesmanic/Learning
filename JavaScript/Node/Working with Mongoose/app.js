d4f0f794dac44581c9391efb89a10b9809a9ec7b
Yesman Choque <yesman.choque@gmail.com>
2021-03-14 14:53:15 -0300

    Adding and Using a User Model

--- JavaScript/Node/Working with Mongoose/app.js
+++ JavaScript/Node/Working with Mongoose/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-/* const User = require('./models/user'); */
+const User = require('./models/user');
 const config = require('./util/config.json');
 
 const app = express();
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+app.use((req, res, next) => {
+  User.findById('604e4d50316346160f76e9d5')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
-}); */
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,6 +38,18 @@ mongoose
     config.connectionLink
   )
   .then(result => {
+    User.findOne().then(user => {
+      if (!user){
+        const user = new User({
+          name: 'Yesman',
+          email: 'yesman.choque@gmail.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch(err => {
--- JavaScript/Node/Working with Mongoose/models/user.js
+++ JavaScript/Node/Working with Mongoose/models/user.js
@@ -1,3 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    cart: {
+        item: [{
+            productId: {
+                type: Schema.Types.ObjectId,
+                required: true
+            },
+            quantity: {
+                type: String,
+                required: true
+            }
+        }]
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
+
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
