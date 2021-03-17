7e0ff03dbb7f226b95f4cc067cc7c120c312f7ea
Yesman Choque <yesman.choque@gmail.com>
2021-03-17 18:32:46 -0300

    Deleting a Cookie

--- JavaScript/Node/Sessions & Cookies/controllers/auth.js
+++ JavaScript/Node/Sessions & Cookies/controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     /* const isLoggedIn = req.get('Cookie').split('=')[1] === 'true' */
     console.log(req.session.isLoggedIn);
@@ -9,6 +11,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('604e55511b8e7124ceb7a11c')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
