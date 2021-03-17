a4ccd7713061ff592bfc7c3167bbac2cb2e759e9
Yesman Choque <yesman.choque@gmail.com>
2021-03-17 18:12:15 -0300

    Using MongoDB to Store Sessions

--- JavaScript/Node/Sessions & Cookies/app.js
+++ JavaScript/Node/Sessions & Cookies/app.js
@@ -4,13 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
-const config = require('./util/config.json');
+const MONGODB_URI = require('./util/config.json').MONGODB_URI;
 const User = require('./models/user');
 
 
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,7 +26,12 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }))
+app.use(session({ 
+  secret: 'my secret', 
+  resave: false, 
+  saveUninitialized: false, 
+  store: store 
+}));
 
 app.use((req, res, next) => {
   User.findById('604e55511b8e7124ceb7a11c')
@@ -40,7 +50,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    config.connectionLink
+    MONGODB_URI
   )
   .then(result => {
     User.findOne().then(user => {
