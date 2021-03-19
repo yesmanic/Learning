c8213e6f9c4e2d1444e7d80e1879fee04dfb7ac1
Yesman Choque <yesman.choque@gmail.com>
2021-03-19 18:35:49 -0300

    Implementing an Authentication Flow

--- JavaScript/Node/Authentication System/app.js
+++ JavaScript/Node/Authentication System/app.js
@@ -55,18 +55,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Yesman',
-          email: 'yesman@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
