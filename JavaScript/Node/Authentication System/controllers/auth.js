e6d5edf292d7cb258ab7e889ce9873a9eba3113f
Yesman Choque <yesman.choque@gmail.com>
2021-03-20 15:43:26 -0300

    Adding the Login Functionality

--- JavaScript/Node/Authentication System/controllers/auth.js
+++ JavaScript/Node/Authentication System/controllers/auth.js
@@ -18,14 +18,29 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('604e55511b8e7124ceb7a11c')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login')
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
 };
