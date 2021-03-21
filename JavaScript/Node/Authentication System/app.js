6d9e2e93082fce0a898fa3169c23fd8bab3f77d6
Yesman Choque <yesman.choque@gmail.com>
2021-03-21 14:08:40 -0300

    Using a CSRF Token

--- JavaScript/Node/Authentication System/app.js
+++ JavaScript/Node/Authentication System/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const MONGODB_URI = require('./util/config.json').MONGODB_URI;
 const errorController = require('./controllers/error');
@@ -15,6 +16,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -33,6 +35,7 @@ app.use(
     store: store
   })
 );
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
