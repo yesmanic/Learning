80ee72222a6f989d610ebf78a72ed29c87b8f93b
Yesman Choque <yesman.choque@gmail.com>
2021-03-21 14:48:38 -0300

    Providing User Feedback

--- JavaScript/Node/Authentication System/app.js
+++ JavaScript/Node/Authentication System/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const MONGODB_URI = require('./util/config.json').MONGODB_URI;
 const errorController = require('./controllers/error');
@@ -36,6 +37,7 @@ app.use(
   })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
