2c7e051f0dd4a8d694ebaaeb756b0d43c284b6a1
Yesman Choque <yesman.choque@gmail.com>
2021-03-28 12:28:21 -0300

    Keeping User Input

--- JavaScript/Node/Validation/controllers/auth.js
+++ JavaScript/Node/Validation/controllers/auth.js
@@ -46,13 +46,29 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: { 
+      email: '', 
+      password: '', 
+      confirmPassword: '' 
+    }
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+	  console.log(errors.array())
+	  return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg
+	  });
+  }
+
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
@@ -88,9 +104,14 @@ exports.postSignup = (req, res, next) => {
   if (!errors.isEmpty()) {
 	  console.log(errors.array())
 	  return res.status(422).render('auth/signup', {
-		path: '/signup',
-		pageTitle: 'Signup',
-		errorMessage: errors.array()[0].msg
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+      oldInput: { 
+        email: email, 
+        password: password, 
+        confirmPassword: req.body.confirmPassword 
+      }
 	  });
   }
   
