ea1dc0383a096a99de2c7c4a478699cd11d57ed2
Yesman Choque <yesman.choque@gmail.com>
2021-04-03 18:07:05 -0300

    Handling File Uploads with Multer

--- JavaScript/Node/File Upload & Download/app.js
+++ JavaScript/Node/File Upload & Download/app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
 
 const MONGODB_URI = require('./util/config.json').MONGODB_URI;
 const errorController = require('./controllers/error');
@@ -27,6 +28,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: 'images' }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
