1e27104ebe1eb70addb02953339fa726382ea82c
Yesman Choque <yesman.choque@gmail.com>
2021-04-06 18:39:00 -0300

    Using PDFKit for PDF Generation

--- JavaScript/Node/File Upload & Download/controllers/shop.js
+++ JavaScript/Node/File Upload & Download/controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -162,6 +164,16 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
+
+      const pdfDoc = new PDFDocument();
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.text('Hello World!');
+
+      pdfDoc.end();
       /* fs.readFile(invoicePath, (err, data) => {
         if (err) {
           return next(err);
@@ -170,10 +182,10 @@ exports.getInvoice = (req, res, next) => {
         res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
         res.send(data);
       }); */
-      const file = fs.createReadStream(invoicePath);
+      /* const file = fs.createReadStream(invoicePath);
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-      file.pipe(res);
+      file.pipe(res); */
     })
     .catch(err => next(err));
 }
