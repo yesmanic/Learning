93f41aee97774ff52eaa5084b3ff0c2b0831ff05
Yesman Choque <yesman.choque@gmail.com>
2021-04-26 17:46:45 -0300

    Fetching a Single Post

--- JavaScript/Node/REST APIs/Backend/controllers/feed.js
+++ JavaScript/Node/REST APIs/Backend/controllers/feed.js
@@ -3,20 +3,19 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'First Post',
-                content: 'This is the first post!',
-                image: 'images/torricelli.png',
-                creator: {
-                    name: 'Yesman'
-                },
-                createdAt: new Date()
+    Post.find()
+        .then(posts => {
+            res.status(200).json({
+                message: 'Fetched posts successfully.',
+                posts
+            })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
             }
-        ]
-    });
+            next(err);
+        });
 };
 
 exports.createPost = (req, res, next) => {
@@ -51,5 +50,27 @@ exports.createPost = (req, res, next) => {
             }
             next(err);
         });
-    
+}
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200)
+                .json({ 
+                    message: 'Post fetched.',
+                    post 
+                });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
