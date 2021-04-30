842e14d912248791573d3cf41ca739fe9ea186c6
Yesman Choque <yesman.choque@gmail.com>
2021-04-30 17:46:16 -0300

    Adding a User Model

--- JavaScript/Node/REST APIs/Backend/app.js
+++ JavaScript/Node/REST APIs/Backend/app.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 
 const MONGODB_URI = require('./util/config.json').MONGODB_URI;
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -50,6 +51,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
