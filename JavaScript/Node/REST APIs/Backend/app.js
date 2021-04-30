e841e15f845ce876502b4879b6a3d2b4c5aba0eb
Yesman Choque <yesman.choque@gmail.com>
2021-04-30 18:03:57 -0300

    Adding User Signup Validation

--- JavaScript/Node/REST APIs/Backend/app.js
+++ JavaScript/Node/REST APIs/Backend/app.js
@@ -57,7 +57,8 @@ app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({ message: message });
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
