19eb1d04f79cf3addf4b4dfc99299af0d8799da3
Yesman Choque <yesman.choque@gmail.com>
2021-05-07 15:13:40 -0300

    Clearing Post-User Relations

--- JavaScript/Node/REST APIs/Backend/controllers/feed.js
+++ JavaScript/Node/REST APIs/Backend/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -15,13 +16,13 @@ exports.getPosts = (req, res, next) => {
             totalItems = count;
             return Post.find()
                 .skip((currentPage - 1) * perPage)
-                .limit(perPage); 
+                .limit(perPage);
         })
         .then(posts => {
             res
                 .status(200)
-                .json({ 
-                    message: 'Fetched posts successfully.', 
+                .json({
+                    message: 'Fetched posts successfully.',
                     posts: posts,
                     totalItems: totalItems
                 });
@@ -49,18 +50,28 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: { name: 'Maximilian' }
+        creator: req.userId
     });
     post
         .save()
         .then(result => {
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created successfully!',
-                post: result
+                post: post,
+                creator: { _id: creator._id, name: creator.name }
             });
         })
         .catch(err => {
@@ -150,18 +161,23 @@ exports.deletePost = (req, res, next) => {
                 throw error;
             }
             if (post.creator.toString() !== req.userId) {
-                const error = new Error('Not authorized');
+                const error = new Error('Not authorized!');
                 error.statusCode = 403;
                 throw error;
             }
+            // Check logged in user
             clearImage(post.imageUrl);
             return Post.findByIdAndRemove(postId);
         })
         .then(result => {
-            console.log(result);
-            res.status(200).json({
-                message: 'Deleted post.'
-            });
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            user.posts.pull(postId);
+            return user.save();
+        })
+        .then(result => {
+            res.status(200).json({ message: 'Deleted post.' });
         })
         .catch(err => {
             if (!err.statusCode) {
