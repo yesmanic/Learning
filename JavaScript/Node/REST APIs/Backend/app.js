fdc58cbf10037022e742a31d3844becb46784898
Yesman Choque <yesman.choque@gmail.com>
2021-05-09 17:21:38 -0300

    Setting Up Socket.io on the Server

--- JavaScript/Node/REST APIs/Backend/app.js
+++ JavaScript/Node/REST APIs/Backend/app.js
@@ -63,6 +63,10 @@ app.use((error, req, res, next) => {
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(result => {
-        app.listen(8080)
+        const server = app.listen(8080);
+        const io = require('socket.io')(server);
+        io.on('connection', socket => {
+            console.log('Client connected');
+        });
     })
     .catch(err => console.log(err));
