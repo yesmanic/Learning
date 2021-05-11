a39c9e087017c90d8a0f8504e2f995cf32a01e11
Yesman Choque <yesman.choque@gmail.com>
2021-05-11 18:27:15 -0300

    Update Frontend

--- JavaScript/Node/REST APIs/Frontend/src/pages/Feed/Feed.js
+++ JavaScript/Node/REST APIs/Frontend/src/pages/Feed/Feed.js
@@ -43,6 +43,20 @@ class Feed extends Component {
     openSocket('http://localhost:8080');
   }
 
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        updatedPosts.pop();
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
